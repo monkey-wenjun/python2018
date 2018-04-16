c3d00ade301c7543b5f6bde118ba27d13b0120eb
方文俊 <fangwenjun@corp.netease.com>
2018-04-16 14:32:48 +0800

    python oop

---
+++ day01/oopdemo.py
@@ -0,0 +1,37 @@
+class Student(object):
+
+
+    def __init__(self,name,score):
+        self.__name = name
+        self.__score = score
+
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+    def print_score(self):
+        print('%s: %s' %(self.name,self.score))
+
+    def set_score(self,score):
+        if 0<=score<=100:
+            self.__score = score
+        else:
+            raise ValueError('bad score')
+
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >=60:
+            return 'B'
+        else:
+            return 'C'
+
+
+
+bart = Student('Bart Simpson',59)
+bart.get_name()
+bart.__name = 'New Name'
+print(bart.__name)
+print(bart.get_name())
